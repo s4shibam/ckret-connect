6337a4281c99dfdaed4e1c8e9e06d4499814fbf6
s4shibam <shibamsaha2002@gmail.com>
2024-01-03 02:49:42 +0530

    update: add new util functions

--- utils/index.js
+++ utils/index.js
@@ -9,3 +9,49 @@ export const generateToken = ({ obj, expiresIn = '1d' }) => {
 export const verifyToken = ({ token }) => {
   return jwt.verify(token, process.env.JWT_SECRET);
 };
+
+export const createUserObjectForSessionToken = (user) => {
+  const {
+    _id,
+    name,
+    email,
+    auth_provider,
+    message_max_length,
+    feedback_message,
+    inbox_max_size,
+    is_inbox_enabled,
+    username
+  } = user;
+
+  const formattedUserObj = {
+    _id,
+    name,
+    email,
+    auth_provider,
+    message_max_length,
+    feedback_message,
+    inbox_max_size,
+    is_inbox_enabled,
+    username
+  };
+  return formattedUserObj;
+};
+
+export const isValidUsername = (username) => {
+  /* 
+    Usernames can only have: 
+    - Lowercase Letters (a-z) 
+    - Uppercase Letters (A-Z) 
+    - Numbers (0-9)
+    - Dots (.)
+    - Underscores (_)
+    - Length: Minimum 5, Maximum 20 characters
+  */
+  const regex = /^[a-zA-Z0-9_\.]+$/;
+
+  const isValidLength = username.length >= 5 && username.length <= 20;
+
+  const isValidPattern = regex.test(username);
+
+  return isValidLength && isValidPattern;
+};
