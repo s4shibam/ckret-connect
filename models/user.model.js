c17627cc93a703f6b7d450d399f8ead69cbf50d7
s4shibam <shibamsaha2002@gmail.com>
2024-01-03 02:51:15 +0530

    update: add new fields in schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 import { Schema, model } from 'mongoose';
 import validator from 'validator';
+import { DEFAULT_CONFIG } from '../constants/index.js';
 
 const schema = new Schema({
   name: {
@@ -8,7 +9,7 @@ const schema = new Schema({
   },
   username: {
     type: String,
-    required: false
+    unique: true
   },
   email: {
     type: String,
@@ -19,6 +20,26 @@ const schema = new Schema({
   auth_provider: {
     type: String,
     required: true
+  },
+  message_max_length: {
+    type: Number,
+    default: DEFAULT_CONFIG.message_max_length
+  },
+  feedback_message: {
+    type: String,
+    default: DEFAULT_CONFIG.feedback_message
+  },
+  inbox_current_size: {
+    type: Number,
+    default: 0
+  },
+  inbox_max_size: {
+    type: Number,
+    default: DEFAULT_CONFIG.inbox_max_size
+  },
+  is_inbox_enabled: {
+    type: Boolean,
+    default: DEFAULT_CONFIG.is_inbox_enabled
   }
 });
 
