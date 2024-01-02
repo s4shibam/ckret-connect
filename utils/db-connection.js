105da782917e292a472c3dafd03affe83a2ac885
s4shibam <shibamsaha2002@gmail.com>
2024-01-03 01:13:40 +0530

    fix: unnecessary log remove

--- utils/db-connection.js
+++ utils/db-connection.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 // Setup connection between MongoDB and the server using mongoose
 export const connectToDB = async () => {
   try {
-    console.log(process.env.DB_URL);
     await mongoose.connect(process.env.DB_URL);
 
     console.log('DB connected'.cyan);
