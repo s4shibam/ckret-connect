4fda7955cdc022cd42f83163a0666e6464aec82d
s4shibam <shibamsaha2002@gmail.com>
2024-01-04 22:59:29 +0530

    feat: add timestamps

--- models/user.model.js
+++ models/user.model.js
@@ -2,45 +2,48 @@ import { Schema, model } from 'mongoose';
 import validator from 'validator';
 import { DEFAULT_CONFIG } from '../constants/index.js';
 
-const schema = new Schema({
-  name: {
-    type: String,
-    required: true
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      unique: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: validator.isEmail
+    },
+    auth_provider: {
+      type: String,
+      required: true
+    },
+    message_max_length: {
+      type: Number,
+      default: DEFAULT_CONFIG.message_max_length
+    },
+    feedback_message: {
+      type: String,
+      default: DEFAULT_CONFIG.feedback_message
+    },
+    inbox_current_size: {
+      type: Number,
+      default: 0
+    },
+    inbox_max_size: {
+      type: Number,
+      default: DEFAULT_CONFIG.inbox_max_size
+    },
+    is_inbox_enabled: {
+      type: Boolean,
+      default: DEFAULT_CONFIG.is_inbox_enabled
+    }
   },
-  username: {
-    type: String,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: validator.isEmail
-  },
-  auth_provider: {
-    type: String,
-    required: true
-  },
-  message_max_length: {
-    type: Number,
-    default: DEFAULT_CONFIG.message_max_length
-  },
-  feedback_message: {
-    type: String,
-    default: DEFAULT_CONFIG.feedback_message
-  },
-  inbox_current_size: {
-    type: Number,
-    default: 0
-  },
-  inbox_max_size: {
-    type: Number,
-    default: DEFAULT_CONFIG.inbox_max_size
-  },
-  is_inbox_enabled: {
-    type: Boolean,
-    default: DEFAULT_CONFIG.is_inbox_enabled
-  }
-});
+  { timestamps: true }
+);
 
 export default model('user', schema);
