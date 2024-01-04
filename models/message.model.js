30df5348f9f23a605f50e2050d4e74be938623cb
s4shibam <shibamsaha2002@gmail.com>
2024-01-04 22:59:53 +0530

    feat: add timestamps and message type field

--- models/message.model.js
+++ models/message.model.js
@@ -1,19 +1,23 @@
 import { Schema, model } from 'mongoose';
+import { MESSAGE_TYPE } from '../constants/index.js';
 
-const schema = new Schema({
-  recipient: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-    required: true
+const schema = new Schema(
+  {
+    recipient: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    message_type: {
+      type: String,
+      default: MESSAGE_TYPE.ANONYMOUS_MESSAGE
+    }
   },
-  content: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 
 export default model('message', schema);
