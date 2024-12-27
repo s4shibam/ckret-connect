68addb6f3fa3cf6eec8037864fe99c8c74c9a374
s4shibam <shibamsaha2002@gmail.com>
2024-12-28 01:36:02 +0530

    refactor: remove legacy code and files for a cleaner codebase

--- middleware/catch-async-error.js
+++
@@ -1,3 +0,0 @@
-export const catchAsyncError = (routeController) => async (req, res, next) => {
-  Promise.resolve(routeController(req, res, next)).catch(next)
-}
--- utils/google-client.js
+++
@@ -1,4 +0,0 @@
-import { OAuth2Client } from 'google-auth-library'
-import { ENV } from '../constants/index.js'
-
-export const googleClient = new OAuth2Client(ENV.google_client_id)
