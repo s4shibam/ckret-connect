debb53f42a1c37c4ff999660721d7e946c847861
s4shibam <shibamsaha2002@gmail.com>
2025-01-03 01:53:05 +0530

    refactor: enhance logging format and mask sensitive data in log messages

--- src/services/log.ts
+++ src/services/log.ts
@@ -6,19 +6,28 @@ import { env } from '../constants/env'
 const logtail = new Logtail(env.logtail_token)
 
 const logFormat = winston.format.combine(
-  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.timestamp({
+    format: () =>
+      new Date().toLocaleString('en-US', {
+        timeZone: 'Asia/Kolkata',
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      }) + ' [IST]'
+  }),
   winston.format.errors({ stack: true }),
-  winston.format.json()
+  winston.format.json(),
+  winston.format.prettyPrint()
 )
 
 const getTransports = (): winston.transport[] => {
   if (env.node_env === 'dev') {
     return [
       new winston.transports.Console({
-        format: winston.format.combine(
-          winston.format.colorize(),
-          winston.format.simple()
-        )
+        format: logFormat
       }),
       new winston.transports.File({
         filename: '.logs/error.log',
@@ -38,11 +47,6 @@ const logger = winston.createLogger({
   level: env.node_env === 'dev' ? 'debug' : 'info',
   format: logFormat,
   transports: getTransports(),
-  exceptionHandlers: [
-    new winston.transports.File({
-      filename: '.logs/exceptions.log'
-    })
-  ],
   exitOnError: false
 })
 
@@ -59,18 +63,26 @@ type TLogMeta = {
 const createLogFunction =
   (level: 'error' | 'warn' | 'info' | 'debug') =>
   (message: string, meta: TLogMeta = {}) => {
-    const formattedReqBody = {
-      ...meta.body,
-      password: meta.body?.password ? '***' : undefined,
-      encrypted_content: meta.body?.encrypted_content ? '***' : undefined,
-      encrypted_reply: meta.body?.encrypted_reply ? '***' : undefined
+    const reqBody = meta.body
+
+    // Mask sensitive data
+    if (reqBody?.password) {
+      reqBody.password = '***'
     }
 
-    logger[level](message, {
-      ...meta,
-      body: formattedReqBody,
-      timestamp: new Date()
-    })
+    if (reqBody?.token) {
+      reqBody.token = reqBody.token.slice(0, 10) + '...'
+    }
+
+    if (reqBody?.messageContent) {
+      reqBody.messageContent = `Length: ${reqBody.messageContent.length}`
+    }
+
+    if (reqBody?.replyContent) {
+      reqBody.replyContent = `Length: ${reqBody.replyContent.length}`
+    }
+
+    logger[level](message, { ...meta, body: reqBody })
   }
 
 export const log = {
